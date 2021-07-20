c2fde2edbfde97b4ab8f0bdab0f8526fd765cb5a
Uki884 <mrchildreny@gmail.com>
2021-07-20 16:35:27 +0900

    startDateとlimitDateを入力したら数字が入るようにした

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,12 +1,38 @@
-import { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
 function Main() {
-  return (
-    <div>
-      <button>テスト</button>
-    </div>
+
+  const [startDate, setStartDate] = React.useState('')
+  const [limitDate, setLimitDate] = React.useState('')
+  const startDateRef = document.getElementById('startDate');
+  const limitDateRef = document.getElementById('limitDate');
+
+  // 工数入力
+  const setEstimatedHours = (value: string) => {
+    console.log('発火')
+    const target: HTMLInputElement = document.getElementById('estimatedHours') as HTMLInputElement;
+    target.value = value
+  }
+
+  // ここで営業日を計算して工数をセットしたい
+  React.useEffect(
+    () => {
+      console.log('effetm', startDate, limitDate);
+      if (startDate && limitDate) {
+        setEstimatedHours(String(222))
+      }
+    },
+    [startDate, limitDate]
   );
+
+  startDateRef?.addEventListener('input', (event: any) => setStartDate(event.target.value));
+  startDateRef?.addEventListener("blur", (event: any) => setStartDate(event.target.value));
+  limitDateRef?.addEventListener('input', (event: any) => setLimitDate(event.target.value));
+  limitDateRef?.addEventListener("blur", (event: any) => setLimitDate(event.target.value));
+  return (<div></div>)
 }
 
-Main()
+const app = document.createElement('div');
+document.body.appendChild(app);
+ReactDOM.render(<Main />, app);
